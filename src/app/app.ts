0ecd459a529c73c0ab6f7b36f7e6d383847b9ffb
Luanhts <luanh3nrique08@gmail.com>
2025-07-28 15:48:54 -0300

    feat: primaryButton component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from './_components/primary-button/primary-button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
