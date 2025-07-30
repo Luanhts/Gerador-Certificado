8a67008bd069b641c345a0258368ce75b3c83050
Luanhts <luanh3nrique08@gmail.com>
2025-07-30 15:30:48 -0300

    feat: ui-base component

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,13 @@ import { CommonModule } from '@angular/common';
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificado } from './_components/item-certificado/item-certificado';
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from './pages/certificados/certificados';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [RouterOutlet, Navbar, CommonModule, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
