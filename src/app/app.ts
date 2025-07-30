422d73cb104e47ccd4a9d82676ef33094996f527
Luanhts <luanh3nrique08@gmail.com>
2025-07-30 14:47:01 -0300

    feat: lista certificados

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
 import { CommonModule } from '@angular/common';
 import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from './_components/item-certificado/item-certificado';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
