fd02deb21fd73ab9e344b8890bd3d48efa669c25
Luanhts <luanh3nrique08@gmail.com>
2025-08-01 08:47:08 -0300

    fix: configs

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,11 +7,12 @@ import { SecondaryButton } from './_components/secondary-button/secondary-button
 import { ItemCertificado } from './_components/item-certificado/item-certificado';
 import { BaseUi } from "./_components/base-ui/base-ui";
 import { Certificados } from './pages/certificados/certificados';
+import { CertificadoForm } from './pages/certificado-form/certificado-form';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, CommonModule, BaseUi, Certificados],
+  imports: [RouterOutlet, Navbar, CommonModule, BaseUi, Certificados, CertificadoForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
